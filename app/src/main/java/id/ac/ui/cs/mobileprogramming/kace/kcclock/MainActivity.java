57dfa67cbe4ee45b44319de036400a277e9c0896
Kevin Christian Chandra <kevin.christian71@ui.ac.id>
2020-10-25 16:05:48 +0700

    Added menu to app bar.

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/MainActivity.java
@@ -3,10 +3,13 @@ package id.ac.ui.cs.mobileprogramming.kace.kcclock;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -41,6 +44,28 @@ public class MainActivity extends AppCompatActivity {
                 setTheme(R.style.DarkTheme);
                 break;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
