131d404f0e460bdec556c02db33f2a1fc6912ae9
Kevin Christian Chandra <kevin.christian71@ui.ac.id>
2020-10-25 14:00:32 +0700

    Initial commit.

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.mobileprogramming.kace.kcclock.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import id.ac.ui.cs.mobileprogramming.kace.kcclock.R;
+
+public class DashboardFragment extends Fragment {
+
+    private DashboardViewModel dashboardViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(DashboardViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        dashboardViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
