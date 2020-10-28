eaf0f5c9d74cf58c837930870e87b4738c54e46e
Kevin Christian Chandra <kevinc.27999@gmail.com>
2020-10-28 22:05:46 +0700

    Created base radio group for selecting alarm type.

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/alarm/AlarmDetailActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/alarm/AlarmDetailActivity.java
@@ -1,10 +1,14 @@
 package id.ac.ui.cs.mobileprogramming.kace.kcclock.alarm;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.RadioButton;
 
 import id.ac.ui.cs.mobileprogramming.kace.kcclock.R;
 
@@ -13,6 +17,9 @@ public class AlarmDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.adjustActivityTheme();
+
         setContentView(R.layout.activity_alarm_detail);
         try
         {
@@ -21,20 +28,57 @@ public class AlarmDetailActivity extends AppCompatActivity {
         catch (NullPointerException e){}
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
+    private void adjustActivityTheme() {
+        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                setTheme(R.style.LightTheme);
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                setTheme(R.style.DarkTheme);
+                break;
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
 
-        if (id == R.id.action_settings) {
-            return true;
+        switch(view.getId()) {
+            case R.id.radioButtonTimeBased:
+                if (checked) {
+                    setSectionVisibility(R.id.sectionEventBased, View.GONE);
+                    setSectionVisibility(R.id.sectionTimeBased, View.VISIBLE);
+                    setRadioButtonText(R.id.radioButtonEventBased, false);
+                    setRadioButtonText(R.id.radioButtonTimeBased, true);
+                }
+                break;
+            case R.id.radioButtonEventBased:
+                if (checked) {
+                    setSectionVisibility(R.id.sectionTimeBased, View.GONE);
+                    setSectionVisibility(R.id.sectionEventBased, View.VISIBLE);
+                    setRadioButtonText(R.id.radioButtonTimeBased, false);
+                    setRadioButtonText(R.id.radioButtonEventBased, true);
+                }
+                break;
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void setSectionVisibility(int id, int value) {
+        View sectionLayout = findViewById(id);
+        sectionLayout.setVisibility(value);
+    }
+
+    private void setRadioButtonText(int id, boolean isActive) {
+        RadioButton radio = findViewById(id);
+
+        TypedValue typedValue = new TypedValue();
+        Resources.Theme theme = getTheme();
+        if (isActive)
+            theme.resolveAttribute(R.attr.activeTextColor, typedValue, true);
+        else
+            theme.resolveAttribute(R.attr.inactiveTextColor, typedValue, true);
+        @ColorInt int color = typedValue.data;
+
+        radio.setTextColor(color);
     }
 }
