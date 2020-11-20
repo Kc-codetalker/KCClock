92f38eff197070ae6b84f2fab9390bb220f06f48
Kevin Christian Chandra <kevinc.27999@gmail.com>
2020-11-20 16:44:59 +0700

    Add switch case for putting intent for two different alarm types.

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/alarm/broadcastReceiver/TimeBasedAlarmReceiver.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/alarm/broadcastReceiver/TimeBasedAlarmReceiver.java
@@ -3,14 +3,21 @@ package id.ac.ui.cs.mobileprogramming.kace.kcclock.alarm.broadcastReceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.BatteryManager;
 import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.util.Calendar;
 
+import id.ac.ui.cs.mobileprogramming.kace.kcclock.alarm.db.Alarm;
+import id.ac.ui.cs.mobileprogramming.kace.kcclock.alarm.db.AppRepository;
+import id.ac.ui.cs.mobileprogramming.kace.kcclock.alarm.db.EventBasedAlarm;
 import id.ac.ui.cs.mobileprogramming.kace.kcclock.alarm.service.AlarmRescheduleService;
 import id.ac.ui.cs.mobileprogramming.kace.kcclock.alarm.service.AlarmRingService;
+
+import static id.ac.ui.cs.mobileprogramming.kace.kcclock.alarm.db.EventBasedAlarm.EVENT_BATTERY_FULL_CHARGE;
+import static id.ac.ui.cs.mobileprogramming.kace.kcclock.application.App.getAppContext;
 
 public class TimeBasedAlarmReceiver extends BroadcastReceiver {
     public static final String MONDAY = "ON MONDAY";
@@ -27,6 +34,10 @@ public class TimeBasedAlarmReceiver extends BroadcastReceiver {
     public static final String VIBRATE = "IS VIBRATE";
     public static final String USE_SOUND = "IS USE SOUND";
     public static final String AUDIO_URI = "AUDIO URI";
+    public static final String EVENT = "EVENT";
+    public static final String ALARM_TYPE = "ALARM TYPE";
+    public static final String TIME_BASED_ALARM = "TIME BASED ALARM";
+    public static final String EVENT_BASED_ALARM = "EVENT BASED ALARM";
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -37,13 +48,28 @@ public class TimeBasedAlarmReceiver extends BroadcastReceiver {
             String toastText = String.format("Alarm Reboot");
             Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show();
             startAlarmRescheduleService(context);
+
+        } else if (Intent.ACTION_BATTERY_CHANGED.equals(action)) {
+            // Start ringing for that alarm
+            // Are we charging / charged?
+            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+            boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING;
+            boolean isFullCharged = status == BatteryManager.BATTERY_STATUS_FULL;
+
+            // CEK DI DB ADA ALARM BATRE YG ENABLED GA
+
+            if (isFullCharged) {
+                String toastText = String.format("Event Based Alarm Received");
+                Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show();
+                startAlarmService(context, intent, EVENT_BASED_ALARM, EVENT_BATTERY_FULL_CHARGE);
+            }
         }
         else {
             // Start ringing for that alarm
             String toastText = String.format("Alarm Received");
             Toast.makeText(context, toastText, Toast.LENGTH_SHORT).show();
             if (!intent.getBooleanExtra(RECURRING, false) || alarmIsToday(intent)) {
-                startAlarmService(context, intent);
+                startAlarmService(context, intent, TIME_BASED_ALARM, null);
             }
         }
     }
@@ -86,17 +112,35 @@ public class TimeBasedAlarmReceiver extends BroadcastReceiver {
         return false;
     }
 
-    private void startAlarmService(Context context, Intent intent) {
+    private void startAlarmService(Context context, Intent intent, String alarmType, String eventType) {
         Intent intentService = new Intent(context, AlarmRingService.class);
-        intentService.putExtra(NAME, intent.getStringExtra(NAME));
-        intentService.putExtra(HOUR, intent.getIntExtra(HOUR, 0));
-        intentService.putExtra(MINUTE, intent.getIntExtra(MINUTE, 0));
-        intentService.putExtra(VIBRATE, intent.getBooleanExtra(VIBRATE, false));
-        intentService.putExtra(USE_SOUND, intent.getBooleanExtra(USE_SOUND, false));
-        intentService.putExtra(AUDIO_URI, intent.getStringExtra(AUDIO_URI));
-        Log.d("Name start service:", intent.getStringExtra(NAME));
-        Log.d("Hour start service:", Integer.toString(intent.getIntExtra(HOUR, 0)));
-        Log.d("Minute start service:", Integer.toString(intent.getIntExtra(MINUTE, 0)));
+        switch (alarmType) {
+            case TIME_BASED_ALARM:
+                intentService.putExtra(ALARM_TYPE, TIME_BASED_ALARM);
+                intentService.putExtra(NAME, intent.getStringExtra(NAME));
+                intentService.putExtra(HOUR, intent.getIntExtra(HOUR, 0));
+                intentService.putExtra(MINUTE, intent.getIntExtra(MINUTE, 0));
+                intentService.putExtra(VIBRATE, intent.getBooleanExtra(VIBRATE, false));
+                intentService.putExtra(USE_SOUND, intent.getBooleanExtra(USE_SOUND, false));
+                intentService.putExtra(AUDIO_URI, intent.getStringExtra(AUDIO_URI));
+                Log.d("Name start service:", intent.getStringExtra(NAME));
+                Log.d("Hour start service:", Integer.toString(intent.getIntExtra(HOUR, 0)));
+                Log.d("Minute start service:", Integer.toString(intent.getIntExtra(MINUTE, 0)));
+                break;
+            case EVENT_BASED_ALARM:
+                AppRepository repo = new AppRepository(getAppContext());
+                Alarm alarm = repo.getEventBasedAlarmByEvent(eventType).getValue();
+                if (((EventBasedAlarm) alarm).isEnabled()) {
+                    intentService.putExtra(ALARM_TYPE, EVENT_BASED_ALARM);
+                    intentService.putExtra(EVENT, ((EventBasedAlarm) alarm).getEvent());
+                    intentService.putExtra(VIBRATE, ((EventBasedAlarm) alarm).isVibrate());
+                    intentService.putExtra(USE_SOUND, ((EventBasedAlarm) alarm).isUseSound());
+                    intentService.putExtra(AUDIO_URI, ((EventBasedAlarm) alarm).getAudioUri());
+                }
+                break;
+            default:
+                return;
+        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             context.startForegroundService(intentService);
