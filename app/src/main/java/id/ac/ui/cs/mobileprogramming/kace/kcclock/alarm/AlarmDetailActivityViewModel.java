89c094102fd127a83da2b6a3b3d679e021fc82e7
Kevin Christian Chandra <kevinc.27999@gmail.com>
2020-11-20 20:57:53 +0700

    Initiate default value for selectedAlarmId.

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/alarm/AlarmDetailActivityViewModel.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/alarm/AlarmDetailActivityViewModel.java
@@ -4,8 +4,14 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import id.ac.ui.cs.mobileprogramming.kace.kcclock.R;
+
 public class AlarmDetailActivityViewModel extends ViewModel {
     private MutableLiveData<Integer> selectedAlarmId = new MutableLiveData<>();
+
+    public AlarmDetailActivityViewModel() {
+        selectedAlarmId.setValue(R.id.radioButtonTimeBased);
+    }
 
     public LiveData<Integer> getSelectedAlarm() {
         return selectedAlarmId;
