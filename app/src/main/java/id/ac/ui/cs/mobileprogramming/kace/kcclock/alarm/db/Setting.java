e10da21b054e90bd8992bdecbe5c9c10f94c8805
Kevin Christian Chandra <kevinc.27999@gmail.com>
2020-11-21 18:32:33 +0700

    Added Setting entity and DAO.

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/alarm/db/Setting.java
@@ -0,0 +1,34 @@
+package id.ac.ui.cs.mobileprogramming.kace.kcclock.alarm.db;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "setting")
+public class Setting {
+    @Ignore
+    public static final String SETTING_LANG = "LANGUAGE";
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "setting_name")
+    private String settingName;
+
+    private String value;
+
+    @NonNull
+    public String getSettingName() {
+        return settingName;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public Setting(String settingName, String value) {
+        this.settingName = settingName;
+        this.value = value;
+    }
+}
---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/alarm/db/SettingDao.java
@@ -0,0 +1,32 @@
+package id.ac.ui.cs.mobileprogramming.kace.kcclock.alarm.db;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface SettingDao {
+    @Query("SELECT * FROM setting ORDER BY setting_name ASC")
+    LiveData<List<Setting>> getAll();
+
+    @Query("SELECT * FROM setting WHERE setting_name LIKE :name LIMIT 1")
+    LiveData<Setting> getSettingByName(String name);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Setting setting);
+
+    @Update
+    void update(Setting setting);
+
+    @Delete
+    void delete(Setting setting);
+
+    @Query("DELETE FROM setting")
+    void deleteAll();
+}
