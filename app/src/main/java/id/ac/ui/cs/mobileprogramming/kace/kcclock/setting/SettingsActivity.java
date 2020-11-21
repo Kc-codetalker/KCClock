61152d5182489d9f538a28ba499798dca4f20d40
Kevin Christian Chandra <kevinc.27999@gmail.com>
2020-11-21 14:57:12 +0700

    Created Settings activity.

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/setting/SettingsActivity.java
@@ -0,0 +1,48 @@
+package id.ac.ui.cs.mobileprogramming.kace.kcclock.setting;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+import id.ac.ui.cs.mobileprogramming.kace.kcclock.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.adjustActivityTheme();
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    private void adjustActivityTheme() {
+        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        switch (currentNightMode) {
+            case Configuration.UI_MODE_NIGHT_NO:
+                setTheme(R.style.LightTheme);
+                break;
+            case Configuration.UI_MODE_NIGHT_YES:
+                setTheme(R.style.DarkTheme);
+                break;
+        }
+    }
+}
