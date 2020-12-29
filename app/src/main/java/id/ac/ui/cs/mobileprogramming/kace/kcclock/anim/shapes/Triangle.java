f521987832180901b16e5f373b1eaa5f1cc5b683
Kevin Christian Chandra <kevinc.27999@gmail.com>
2020-12-29 16:35:07 +0700

    Added draw logic for triangle.

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/anim/shapes/Triangle.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/kace/kcclock/anim/shapes/Triangle.java
@@ -74,4 +74,38 @@ public class Triangle {
         // creates OpenGL ES program executables
         GLES20.glLinkProgram(mProgram);
     }
+
+    private int positionHandle;
+    private int colorHandle;
+
+    private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
+
+    public void draw() {
+        // Add program to OpenGL ES environment
+        GLES20.glUseProgram(mProgram);
+
+        // get handle to vertex shader's vPosition member
+        positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
+        // Enable a handle to the triangle vertices
+        GLES20.glEnableVertexAttribArray(positionHandle);
+
+        // Prepare the triangle coordinate data
+        GLES20.glVertexAttribPointer(positionHandle, COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT, false,
+                vertexStride, vertexBuffer);
+
+        // get handle to fragment shader's vColor member
+        colorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+
+        // Set color for drawing the triangle
+        GLES20.glUniform4fv(colorHandle, 1, color, 0);
+
+        // Draw the triangle
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+
+        // Disable vertex array
+        GLES20.glDisableVertexAttribArray(positionHandle);
+    }
 }
